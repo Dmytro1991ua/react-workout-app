fb29386ab52bc8be6b36add4a730cef37babbec7
Dmytro Kurchenko <dmytro.kurchenko@sigma.software>
2022-02-06 18:31:21 +0200

    Show logout button when user is logged in

--- src/modules/Header/components/Navigation/Navigation.config.tsx
+++ src/modules/Header/components/Navigation/Navigation.config.tsx
@@ -1,26 +1,30 @@
+import firebase from 'firebase/app';
+
 import { AppRoutes } from '../../../../App.enums';
 import { authService } from '../../../Auth/Auth.service';
 import { CustomDumbbellIcon, CustomHomeIcon, CustomLogoutIcon } from '../../Header.styled';
 import { NavigationConfiguration } from './Navigation.interface';
 
-export const navigationConfig: NavigationConfiguration[] = [
-  {
-    id: 0,
-    navigationIcon: <CustomHomeIcon />,
-    url: AppRoutes.Home,
-    'data-tip': 'Home',
-  },
-  {
-    id: 1,
-    navigationIcon: <CustomDumbbellIcon />,
-    url: AppRoutes.Workouts,
-    'data-tip': 'Workouts',
-  },
-  {
-    id: 2,
-    navigationIcon: <CustomLogoutIcon />,
-    url: AppRoutes.Login,
-    'data-tip': 'Logout',
-    onClick: () => authService.logout(),
-  },
-];
+export function navigationConfig(currentUser: firebase.User): NavigationConfiguration[] {
+  return [
+    {
+      id: 0,
+      navigationIcon: <CustomHomeIcon />,
+      url: AppRoutes.Home,
+      'data-tip': 'Home',
+    },
+    {
+      id: 1,
+      navigationIcon: <CustomDumbbellIcon />,
+      url: AppRoutes.Workouts,
+      'data-tip': 'Workouts',
+    },
+    currentUser && {
+      id: 2,
+      navigationIcon: <CustomLogoutIcon />,
+      url: AppRoutes.Login,
+      'data-tip': 'Logout',
+      onClick: () => authService.logout(),
+    },
+  ];
+}
--- src/modules/Header/components/Navigation/Navigation.tsx
+++ src/modules/Header/components/Navigation/Navigation.tsx
@@ -6,24 +6,28 @@ import { WorkoutsContext } from '../../../../context/WorkoutsContext';
 import { navigationConfig } from './Navigation.config';
 import Tooltip from './../../../../components/Tooltip/Tooltip';
 import { colors } from '../../../../global-styles/Global.styled';
+import { AuthContext } from './../../../../context/AuthContext';
 
 const Navigation = () => {
   //destructure burger menu and navigation "state"
+  const { currentUser } = useContext(AuthContext);
   const { show } = useContext(WorkoutsContext);
   const [open] = show;
 
+  const navigationConfigs = navigationConfig(currentUser);
+
   return (
     <List open={open}>
-      {navigationConfig.map((navLink) => {
+      {navigationConfigs.map((navLink) => {
         return (
-          <ListItem key={navLink.id}>
+          <ListItem key={navLink && navLink.id}>
             <Link
-              key={navLink.id}
-              data-tip={navLink['data-tip']}
-              to={{ pathname: navLink.url }}
-              onClick={navLink.onClick}
+              key={navLink && navLink.id}
+              data-tip={navLink && navLink['data-tip']}
+              to={{ pathname: navLink && navLink.url }}
+              onClick={navLink && navLink.onClick}
             >
-              {navLink.navigationIcon}
+              {navLink && navLink.navigationIcon}
             </Link>
             <Tooltip
               effect='solid'
