7198c308697c3cb51dbe4c598930859b1c78a300
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2022-03-20 15:49:34 +0200

    Add marker with current location on first render, adjust logic when initial marker is shown and hidden (#57)
    Co-authored-by: Dmytro Kurchenko <dmytro.kurchenko@sigma.software>

--- src/modules/Workouts/Workouts.constants.ts
+++ src/modules/Workouts/Workouts.constants.ts
@@ -1,3 +1,6 @@
+import L from 'leaflet';
+import InitialWorkoutMarker from '../../assets/images/leaflet/marker.png';
+
 export const WORKOUT_SUCCESS_DELETE_MESSAGE = 'Your workout has been successfully deleted';
 
 export const MONTHS_LIST = [
@@ -23,3 +26,10 @@ export const WORKOUT_TYPE_SELECTION_OPTIONS_MOCK = [
 export const MIN_MARKER_POP_UP_WIDTH = 250;
 export const MAX_MARKER_POP_UP_WIDTH = 350;
 export const ZOOM_LEVEL = 13; //default zoom level
+
+export const INITIAL_MAP_MARKER = new L.Icon({
+  iconUrl: InitialWorkoutMarker,
+  iconSize: [50, 55],
+  iconAnchor: [0, 60],
+  popupAnchor: [26, -60],
+});
--- src/modules/Workouts/Workouts.tsx
+++ src/modules/Workouts/Workouts.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext, useState } from 'react';
+import React, { ReactElement, useContext, useEffect, useState } from 'react';
 
 import { FeaturesTitle, WorkoutsFeatures, Map, WorkoutsSection, WorkoutsSectionBody } from './Workouts.styled';
 import { WorkoutsContext, WorkoutsProps } from '../../context/WorkoutsContext';
@@ -6,13 +6,17 @@ import Form from './components/WorkoutForm/Form';
 import Workout from './components/Workout/Workout';
 import WorkoutsMap from './components/WorkoutsMap';
 import FallbackMessage from './components/FallbackMessage/FallbackMessage';
-import { LatLngTuple } from 'leaflet';
+import { LatLngExpression, LatLngTuple } from 'leaflet';
+import useGeolocation from '../../hooks/useGeolocation';
+import InitialMapMarker from './components/MapMarker/InitialMapMarker';
 
 const Workouts = (): ReactElement => {
   // destructure certain "states" from Context
   const { workoutsData } = useContext(WorkoutsContext);
-
   const [workouts] = workoutsData;
+
+  const location = useGeolocation();
+  const currentPosition: LatLngExpression = [location.coordinates.lat, location.coordinates.lng];
 
   const [isFormShown, setIsFormShown] = useState(false);
 
@@ -33,6 +37,10 @@ const Workouts = (): ReactElement => {
   // sort out a workouts array with object in order to display last added object to an array as a first in a list
   const sortedWorkouts = workouts.sort((obj1: any, obj2: any) => obj2.id - obj1.id);
 
+  useEffect(() => {
+    <InitialMapMarker position={currentPosition} />;
+  }, []);
+
   return (
     <WorkoutsSection>
       <WorkoutsSectionBody>
@@ -51,7 +59,11 @@ const Workouts = (): ReactElement => {
           ))}
         </WorkoutsFeatures>
         <Map>
-          <WorkoutsMap onShowWorkoutForm={showWorkoutForm} setMapCoords={setClickedMapCoordinates} />
+          <WorkoutsMap
+            onShowWorkoutForm={showWorkoutForm}
+            setMapCoords={setClickedMapCoordinates}
+            isFormShown={isFormShown}
+          />
         </Map>
       </WorkoutsSectionBody>
     </WorkoutsSection>
