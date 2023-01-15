ea386a8b1fe81f1924c578cf64f88de7ef576733
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2023-01-15 13:26:01 +0200

    Increase quiz timer based on selected question quantity (#113)

--- frontend/src/modules/WorkoutsQuiz/WorkoutQuiz.constants.ts
+++ frontend/src/modules/WorkoutsQuiz/WorkoutQuiz.constants.ts
@@ -4,7 +4,6 @@ export const QUiZ_INTRODUCTION_TITLE =
   'Challenge yourself and check your real knowledge in exclusive Workouts, Fitness, Healthy and Lifestyle quiz';
 
 export const QUIZ_ENDING_MESSAGE = 'The quiz has ended due to time running out';
-export const QUIZ_DEFAULT_TIMER = 60;
 export const DEFAULT_QUIZ_QUESTIONS_QUANTITY = 10;
 export const QUIZ_QUESTIONS_OPTIONS: SelectedOption[] = [
   {
---
+++ frontend/src/modules/WorkoutsQuiz/WorkoutQuiz.enums.ts
@@ -0,0 +1,19 @@
+export enum SelectedQuestionQuantity {
+  DefaultQuestionQuantity = '',
+  FifteenQuestions = '15',
+  TwentyQuestions = '20',
+  TwentyFiveQuestions = '25',
+  ThirtyQuestions = '30',
+  ThirtyFiveQuestions = '35',
+  FortyQuestions = '40',
+}
+
+export enum QuizTimer {
+  OneMinute = 60,
+  MinuteAndAHalf = 90,
+  TwoMinutes = 120,
+  TwoMinutesAndHalf = 150,
+  ThreeMinutes = 180,
+  ThreeMinutesAndHalf = 210,
+  FourMinutes = 240,
+}
